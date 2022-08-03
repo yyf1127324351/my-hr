099e604d67578caf0c1faa1d09531bfe397fe881
yfyang <yfyang@i9i8.com>
2022-08-03 16:23:00 +0800

    修改表名

---
+++ src/main/java/com/myhr/hr/model/UserDto.java
@@ -0,0 +1,16 @@
+package com.myhr.hr.model;
+
+import lombok.Data;
+
+/**
+ * @Description
+ * @Author yyf
+ * @Date 2022-07-06 15:41
+ */
+@Data
+public class UserDto {
+    private Long id;
+    private String userName;
+    private String loginName;
+
+}
